c9fe76634534ea5d31711b46062145c45e5466f7
AhlaamK-tap <a.kazi@tap.company>
2022-08-05 15:42:01 +0300

    trial
    updated ui kit
    heght wrap
    topmargin -21

--- app/src/main/java/company/tap/InlineCardInput/MainActivity.kt
+++ app/src/main/java/company/tap/InlineCardInput/MainActivity.kt
@@ -2,18 +2,27 @@ package company.tap.InlineCardInput
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 
 import company.tap.cardinputwidget.widget.inline.CardInlineForm
 import company.tap.cardinputwidget.widget.inline.InlineCardInput
+import company.tap.tapuilibrary.themekit.ThemeManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var cardInlineForm:InlineCardInput
+    lateinit var  paymentInputContainer: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //cardInlineForm =findViewById(R.id.cardInlineForm)
+        ThemeManager.loadTapTheme(this.resources,R.raw.defaulttheme,"ligththeme")
         setContentView(R.layout.activity_main)
 
-        cardInlineForm =findViewById(R.id.cardInlineForm)
+        cardInlineForm = InlineCardInput(this)
+        paymentInputContainer = findViewById(R.id.payment_input_layout)
         cardInlineForm.holderNameEnabled= true
+        paymentInputContainer.addView(cardInlineForm)
     }
 }

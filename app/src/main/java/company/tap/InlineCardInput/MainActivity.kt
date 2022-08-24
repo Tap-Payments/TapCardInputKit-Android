7dd5ed9d4cced140b91be09bf8eef1c41abcb068
AhlaamK-tap <a.kazi@tap.company>
2022-08-24 11:37:51 +0300

    scroll

--- app/src/main/java/company/tap/InlineCardInput/MainActivity.kt
+++ app/src/main/java/company/tap/InlineCardInput/MainActivity.kt
@@ -1,5 +1,6 @@
 package company.tap.InlineCardInput
 
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.LinearLayout
@@ -14,19 +15,26 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
     lateinit var cardInlineForm:InlineCardInput
     lateinit var  paymentInputContainer: LinearLayout
+    lateinit var  mainView: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         //cardInlineForm =findViewById(R.id.cardInlineForm)
 
-        ThemeManager.loadTapTheme(this.resources,R.raw.defaultdarktheme,"darktheme")
-        LocalizationManager.setLocale(this, Locale("ar"))
+        ThemeManager.loadTapTheme(this.resources,R.raw.defaultlighttheme,"lighttheme")
+        LocalizationManager.setLocale(this, Locale("en"))
         setContentView(R.layout.activity_main)
 
         cardInlineForm = InlineCardInput(this)
         paymentInputContainer = findViewById(R.id.payment_input_layout)
+        mainView = findViewById(R.id.mainView)
         cardInlineForm.holderNameEnabled= false
         paymentInputContainer.addView(cardInlineForm)
+
+        mainView.setOnTouchListener { v, event ->
+            cardInlineForm.onTouchView()
+            true
+        }
     }
 }
